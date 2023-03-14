71dd978399fefbc147d3d3dc7b9088a507a59467
Emma Engvall <emma.engvall@ica.se>
2023-03-14 19:51:47 +0100

    Example project week 6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Name } from 'components/Name.js';
+import { Food } from 'components/Food.js';
+import { Drink } from 'components/Drink.js';
+import { Result } from 'components/Result.js';
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
+  const [step, setStep] = useState(1);
+  const [name, setName] = useState('');
+  const [food, setFood] = useState('');
+  const [drink, setDrink] = useState('');
+
+  const handleStepIncrease = () => {
+    setStep(step + 1);
+  }
+
+    return (
+      <>
+        {step === 1 && (
+          <Name name={name} setName={setName} />
+        )}
+
+        {step === 2 && (
+         <Food food={food} setFood={setFood} />
+        )}
+
+        {step === 3 && (
+          <Drink drink={drink} setDrink={setDrink} />
+        )}
+
+        {step >= 4 && (
+          <Result name={name} food={food} drink={drink} />      
+        )}
+
+
+        {step < 4 && (
+          <>
+            <p>Current step: {step}</p>
+            <p>Current name: {name}</p>
+            <button type="button" onClick={handleStepIncrease}>Next step 🦶</button>
+          </>
+        )}  
+    </>
+  );
 }
